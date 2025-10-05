337ffe69f0b94f77774cb7d89dbf7d8b62b48756
Mariya Rajiv <166097259+MariyRajiv@users.noreply.github.com>
2025-10-05 15:40:15 +0530

    Update AddEditBookPage.js

--- frontend/src/pages/AddEditBookPage.js
+++ frontend/src/pages/AddEditBookPage.js
@@ -29,7 +29,7 @@ const AddEditBookPage = () => {
   const [pageLoading, setPageLoading] = useState(isEditMode);
   const [genres, setGenres] = useState([]);
 
- const BACKEND_URL = "http://localhost:5000"; // local backend
+ const BACKEND_URL = "https://book-review-platform-1-htw9.onrender.com"; // local backend
 
   const commonGenres = [
     'Fiction', 'Non-Fiction', 'Mystery', 'Romance', 'Science Fiction', 'Fantasy',
